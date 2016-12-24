b6ce229bffd8ab96f58366a95a1c3a197336af01
mohsen <mohsenhq@gmail.com>
2016-12-24 17:37:11 +0330

    module added

--- app/src/main/java/com/example/mahtak/mapp/MainActivity.java
+++ app/src/main/java/com/example/mahtak/mapp/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.mahtak.mapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.mahtak.LifeCycleReporter;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        LifeCycleReporter reporter = new LifeCycleReporter();
+        getApplication().registerActivityLifecycleCallbacks(reporter);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
