ff0292fb0f718f9258e9facdaccf581613d2c098
mohsen <mohsenhq@gmail.com>
2017-01-09 17:42:15 +0330

    stacktrace added

--- app/src/main/java/com/example/mahtak/mapp/MainActivity.java
+++ app/src/main/java/com/example/mahtak/mapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.mahtak.mapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 import com.example.mahtak.LifeCycleReporter;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_crash).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String crashString = null;
+                crashString.length();
+            }
+        });
     }
 }
