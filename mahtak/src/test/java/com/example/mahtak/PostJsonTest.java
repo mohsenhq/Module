0e48ee47170a3fab98f16326582e29624a6de310
mohsenhq <mohsenhq@gmail.com>
2018-10-02 17:04:24 +0330

    PostJson test fixed

--- mahtak/src/test/java/com/example/mahtak/PostJsonTest.java
+++ mahtak/src/test/java/com/example/mahtak/PostJsonTest.java
@@ -1,4 +1,4 @@
-//package com.example.mahtak;
+package com.example.mahtak;//package com.example.mahtak;
 //
 //import org.json.JSONArray;
 //import org.json.JSONObject;
@@ -70,3 +70,56 @@
 //
 //
 //}
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public class PostJsonTest {
+
+    @Test
+    public void databaseSendTest() throws Exception {
+
+        String testString = "{\"index\":1,\"index_start_at\":56,\"integer\":47,\"float\":18.5305,\"name\":\"Teresa\",\"surname\":\"Beach\",\"fullname\":\"Grace Case\",\"email\":\"sheryl@brady.ae\",\"bool\":\"false\"}";
+
+        JSONObject testJson = new JSONObject(testString);
+
+        System.out.println("hello :" + testJson.toString());
+
+
+        new PostJson().postData(testJson.toString());
+
+        //Receive
+        //Get last data entered in database
+        URL url = new URL("http://81.31.168.201:8081");
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        httpURLConnection.setRequestProperty("Content-Type", "application/json"); // data type = json
+        httpURLConnection.connect();
+        BufferedReader in = null;
+        in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+        JSONArray jsonArray = new JSONArray(in.readLine());
+        JSONObject jsonObjectReceived = jsonArray.getJSONObject(0);
+        jsonObjectReceived.remove("_id");
+
+        //Put all received data in one string
+//        receivedString += jsonObjectReceived.toString() + ";";
+
+
+        //Check if send and recived data are the same
+        assertEquals("not the same",jsonObjectReceived.toString(),testJson.toString());
+    }
+
+
+}
